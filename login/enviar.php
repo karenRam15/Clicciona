41c5221229a9dcc113ece565216fc46d48b4ccf3
karenRam15 <karen_re@tesch.edu.mx>
2022-06-04 21:49:31 -0500

    Enviar correo (phpmailer)

--- login/enviar.php
+++ login/enviar.php
@@ -1,14 +1,38 @@
-<?php 
-	$to = "karen_re@tesch.edu.mx";
-    $subject = "Password Update Request";
-    $mailContent = 'Holis';
-                //set content-type header for sending HTML email
-                $headers = "MIME-Version: 1.0" . "rn";
-                $headers .= "Content-type:text/html;charset=UTF-8" . "rn";
-                //additional headers
-                $headers .= 'From: Tu<[email protected]>' . "rn";
-                //send email
-                mail('karen_re@tesch.edu.mx','Titulo',$mailContent);
-                
-                $sessData['status']['type'] = 'success';
-                $sessData['status']['msg'] = 'Please check your e-mail, we have sent a password reset link to your registered email.';
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../phpmiler/src/Exception.php';
+require '../phpmiler/src/PHPMailer.php';
+require '../phpmiler/src/SMTP.php';
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'enrique_rr@tesch.edu.mx';                     //SMTP username
+    $mail->Password   = 'F4m1l14.R4m0s';                               //SMTP password
+    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('karen_re@tesch.edu.mx', 'Clicciona');
+    $mail->addAddress('karen_re@tesch.edu.mx', 'Enrique');     //Add a recipient              
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Creación de usuario';
+    $mail->Body    = 'HOLA';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
